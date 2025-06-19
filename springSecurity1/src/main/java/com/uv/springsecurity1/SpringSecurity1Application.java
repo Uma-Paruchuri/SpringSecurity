509243012036311157b358dfdd8227b80ce32fc3
Uma-Paruchuri <umavenkatesenn@gmail.com>
2025-06-18 22:50:52 -0400

    Added custom UsernamePasswordAuth Provider.

--- springSecurity1/src/main/java/com/uv/springsecurity1/SpringSecurity1Application.java
+++ springSecurity1/src/main/java/com/uv/springsecurity1/SpringSecurity1Application.java
@@ -3,8 +3,6 @@ package com.uv.springsecurity1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
 @SpringBootApplication
 public class SpringSecurity1Application {
 
